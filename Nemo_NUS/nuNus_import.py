47b6c953db291f452aac319bb153df086ea65280
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2023-06-05 13:22:52 -0700

    still stuck getting correct face data

--- Nemo_NUS/nuNus_import.py
+++ Nemo_NUS/nuNus_import.py
@@ -1,43 +1,58 @@
 from struct import unpack
-import bpy
-        
+import bpy        
 
-def ReadNUS_TSG(f, vertices=[], faces=[]):
+def ReadNUS_pointclouds_one(f, vertices=[]):
     f.seek(0)
-    while 1:
+    ChunkRead = f.read()
+    f.seek(0)
+    for i in range(len(ChunkRead)):
         Chunk = f.read(4)
         if Chunk == b"0TSG":
             Size = unpack(">I", f.read(4))[0]
             ObjectCount = unpack(">I", f.read(4))[0]
             for i in range(int(Size)-12):
-                f.seek(24,1)
-                #offset = f.read(24)
-                #if offset == b"\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01":
-                unknown = unpack(">I", f.read(4))[0]
-                vertexCount = unpack(">I", f.read(4))[0]
-                for i in range(vertexCount):
-                    vx = unpack(">f", f.read(4))[0]
-                    vy = unpack(">f", f.read(4))[0]
-                    vz = unpack(">f", f.read(4))[0]
-                    r = unpack("B", f.read(1))[0] / 255.0
-                    g = unpack("B", f.read(1))[0] / 255.0
-                    b = unpack("B", f.read(1))[0] / 255.0
-                    a = unpack("B", f.read(1))[0] / 255.0
-                    uvx = unpack(">f", f.read(4))[0]
-                    uvy = unpack(">f", f.read(4))[0]
-                    vertices.append([vx,vy,vz])
-                f.seek(16,1)
-                FaceSize = unpack(">I", f.read(4))[0]
-                Facetype_ = unpack("B", f.read(1))[0]
-                if Facetype_ == 1:
-                    pass
-                elif Facetype_ == 0:
-                    pass
-            break
-        elif Chunk == b"SDNB":
-            break
+                offset = f.read(24)
+                if offset == b"\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01":
+                    unknown = unpack(">I", f.read(4))[0]
+                    vertexCount = unpack(">I", f.read(4))[0]
+                    for i in range(vertexCount):
+                        vx = unpack(">f", f.read(4))[0]
+                        vy = unpack(">f", f.read(4))[0]
+                        vz = unpack(">f", f.read(4))[0]
+                        r = unpack("B", f.read(1))[0] / 255.0
+                        g = unpack("B", f.read(1))[0] / 255.0
+                        b = unpack("B", f.read(1))[0] / 255.0
+                        a = unpack("B", f.read(1))[0] / 255.0
+                        uvx = unpack(">f", f.read(4))[0]
+                        uvy = unpack(">f", f.read(4))[0]
+                        vertices.append([vx,vy,vz])
+
+                    readOne = f.read(60)
+                    f.seek(-60,1)
+                    readTwo = f.read(12)
+                    f.seek(-12,1)
+                    if len(readOne) == 60:
+                        
+                        f.seek(16,1)
+                        FaceSize = unpack(">I", f.read(4))[0]
+                        Facetype_ = unpack("B", f.read(1))[0]
+                        f.seek(36,1)
+                        if Facetype_ == 0:
+                            #unsigned short with 3 bytes
+                            f.seek(1,1)
+                            facecount = unpack(">H", f.read(2))[0]
+                        elif Facetype_ == 1:
+                            f.seek(1,1)
+                            facecount = unpack(">H", f.read(2))[0]
+                            #unsigned with 3 bytes
+                    if len(readTwo) == 12:
+                        cntrl = unpack(">I", f.read(4))[0]
         
     mesh = bpy.data.meshes.new("dragonjan")
-    mesh.from_pydata(vertices, [], faces)
+    mesh.from_pydata(vertices, [], [])
     object = bpy.data.objects.new("dragonjan", mesh)
     bpy.context.collection.objects.link(object)
+
+def NUSRead(filepath):
+    with open(filepath, "rb") as f:
+        ReadNUS_pointclouds_one(f, vertices=[])
