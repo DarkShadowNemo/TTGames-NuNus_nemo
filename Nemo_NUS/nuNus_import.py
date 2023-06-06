00a4b1f6819bb8f15b56a1e07d5891e78a259d3c
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2023-06-06 12:53:36 -0700

    same as hgo and nus, needs some testing and debugging

--- Nemo_NUS/nuNus_import.py
+++ Nemo_NUS/nuNus_import.py
@@ -29,25 +29,118 @@ def ReadNUS_pointclouds_one(f, vertices=[]):
 
                     readOne = f.read(60)
                     f.seek(-60,1)
-                    readTwo = f.read(12)
-                    f.seek(-12,1)
                     if len(readOne) == 60:
                         
                         f.seek(16,1)
                         FaceSize = unpack(">I", f.read(4))[0]
                         Facetype_ = unpack("B", f.read(1))[0]
-                        f.seek(36,1)
                         if Facetype_ == 0:
-                            #unsigned short with 3 bytes
+                            f.seek(1,1) # eight
+                            f.seek(1,1) # 0xA0 flag
+                            ############
+                            #zero
                             f.seek(1,1)
-                            facecount = unpack(">H", f.read(2))[0]
+                            f.seek(1,1)
+                            f.seek(1,1)
+                            f.seek(1,1)
+                            #############
+                            f.seek(1,1) # eight
+                            f.seek(1,1) # 0xB0 flag
+                            #############
+                            #zero
+                            f.seek(1,1)
+                            f.seek(1,1)
+                            f.seek(1,1)
+                            #############
+                            repeatedfafbfc = unpack("B", f.read(1))[0]
+                            if repeatedfafbfc == 36:
+                                f.seek(1,1) # eight
+                                f.seek(1,1) # 0xA2 flag
+                                ###########
+                                #zero
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                ############
+                                f.seek(1,1) # eight
+                                f.seek(1,1) # 0xB2 flag
+                                ############
+                                #zero
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                f.seek(1,1)
+                            elif repeatedfafbfc == 24:
+                                f.seek(1,1) # eight
+                                f.seek(1,1) # 0xA2 flag
+                                ###########
+                                #zero
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                ############
+                                f.seek(1,1) # eight
+                                f.seek(1,1) # 0xB2 flag
+                                ############
+                                #zero
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                f.seek(1,1)
                         elif Facetype_ == 1:
+                            f.seek(1,1) # eight
+                            f.seek(1,1) # 0xA0 flag
+                            ############
+                            #zero
                             f.seek(1,1)
-                            facecount = unpack(">H", f.read(2))[0]
-                            #unsigned with 3 bytes
-                    if len(readTwo) == 12:
-                        cntrl = unpack(">I", f.read(4))[0]
-        
+                            f.seek(1,1)
+                            f.seek(1,1)
+                            f.seek(1,1)
+                            #############
+                            f.seek(1,1) # eight
+                            f.seek(1,1) # 0xB0 flag
+                            #############
+                            #zero
+                            f.seek(1,1)
+                            f.seek(1,1)
+                            f.seek(1,1)
+                            #############
+                            repeatedfafbfc = unpack("B", f.read(1))[0]
+                            if repeatedfafbfc == 36:
+                                f.seek(1,1) # eight
+                                f.seek(1,1) # 0xA2 flag
+                                ###########
+                                #zero
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                ############
+                                f.seek(1,1) # eight
+                                f.seek(1,1) # 0xB2 flag
+                                ############
+                                #zero
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                f.seek(1,1)
+                            elif repeatedfafbfc == 24:
+                                f.seek(1,1) # eight
+                                f.seek(1,1) # 0xA2 flag
+                                ###########
+                                #zero
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                ############
+                                f.seek(1,1) # eight
+                                f.seek(1,1) # 0xB2 flag
+                                ############
+                                #zero
+                                f.seek(1,1)
+                                f.seek(1,1)
+                                f.seek(1,1)
+                            
     mesh = bpy.data.meshes.new("dragonjan")
     mesh.from_pydata(vertices, [], [])
     object = bpy.data.objects.new("dragonjan", mesh)
