ba7f68e0d5a2bf9fd1a92c059a412f3120e137fb
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2023-07-24 20:39:40 -0700

    TODO loop the correct face data, algorithm been cracked already

--- Nemo_NUS/nuNus_import.py
+++ Nemo_NUS/nuNus_import.py
@@ -28,22 +28,99 @@ def ReadNUS_pointclouds_one(f, vertices=[], faces=[]):
                         uvy = unpack(">f", f.read(4))[0]
                         vertices.append([vx,vy,vz])
 
-                    f.seek(16,1)
-                    FaceSize = unpack(">I", f.read(4))[0]
-                    #facecount loop
-                    #for i in range(facecount-2):
-                    fa = unpack(">H", f.read(2))[0] >> 8
-                    f.seek(1,1)
-                    fb = unpack(">H", f.read(2))[0] >> 8
-                    f.seek(1,1)
-                    fc = unpack(">H", f.read(2))[0] >> 8
-                    f.seek(1,1)
-                    f.seek(-4,1)
+                    unk1 = unpack(">H", f.read(2))[0]
+                    unk2 = unpack("B", f.read(1))[0]
+                    unk3 = unpack(">H", f.read(2))[0]
+                    unk4 = unpack("B", f.read(1))[0]
+                    unk5 = unpack(">H", f.read(2))[0]
+                    unk6 = unpack("B", f.read(1))[0]
+                    unk7 = unpack(">H", f.read(2))[0]
+                    unk8 = unpack("B", f.read(1))[0]
+                    unk9 = unpack(">H", f.read(2))[0]
+                    unk10 = unpack("B", f.read(1))[0]
+                    unk11 = unpack(">H", f.read(2))[0]
+                    unk12 = unpack("B", f.read(1))[0]
+                    FaceSize = unpack(">H", f.read(2))[0]
+                    type1 = unpack("B", f.read(1))[0]
+                    if type1==1:
+                        
+                        unknown_entrysize1 = unpack(">H", f.read(2))[0]#2
+                        unknown = unpack("B", f.read(1))[0]#3
+                        unknown2 = unpack(">H", f.read(2))[0]#5
+                        unknown3 = unpack("B", f.read(1))[0]#6
+                        unknown_entrysize2 = unpack(">H", f.read(2))[0]#8
+                        unknown4 = unpack("B", f.read(1))[0]#9
+                        unknown5 = unpack(">H", f.read(2))[0]#11
+                        unknown6 = unpack("B", f.read(1))[0]#12
+                        unknown7 = unpack(">H", f.read(2))[0]#14
+                        unknown8 = unpack("B", f.read(1))[0]#15
+                        unknown9 = unpack(">H", f.read(2))[0]#17
+                        unknown10 = unpack("B", f.read(1))[0]#18
+                        unknown11 = unpack(">H", f.read(2))[0]#20
+                        unknown12 = unpack("B", f.read(1))[0]#21
+                        unknown13 = unpack(">H", f.read(2))[0]#23
+                        unknown14 = unpack("B", f.read(1))[0]#24
+                        unknown15 = unpack(">H", f.read(2))[0]#26
+                        unknown16 = unpack("B", f.read(1))[0]#27
+                        unknown17 = unpack(">H", f.read(2))[0]#29
+                        unknown18 = unpack("B", f.read(1))[0]#30
+                        unknown19 = unpack(">H", f.read(2))[0]#32
+                        unknown20 = unpack("B", f.read(1))[0]#33
+                        unknown21 = unpack(">H", f.read(2))[0]#35
+                        unknown22 = unpack("B", f.read(1))[0]#33
+                        #TODO negative 0x98
+                        facecount = unpack("B", f.read(1))[0]
+                        #facecount loop
+                        for i in range(facecount-2):
+                            fa = unpack(">H", f.read(2))[0] >> 8
+                            f.seek(1,1)
+                            fb = unpack(">H", f.read(2))[0] >> 8
+                            f.seek(1,1)
+                            fc = unpack(">H", f.read(2))[0] >> 8
+                            f.seek(1,1)
+                            f.seek(-4,1)
+                    elif type1==0:
+                        
+                        unknown_entrysize1 = unpack(">H", f.read(2))[0]#2
+                        unknown = unpack("B", f.read(1))[0]#3
+                        unknown2 = unpack(">H", f.read(2))[0]#5
+                        unknown3 = unpack("B", f.read(1))[0]#6
+                        unknown_entrysize2 = unpack(">H", f.read(2))[0]#8
+                        unknown4 = unpack("B", f.read(1))[0]#9
+                        unknown5 = unpack(">H", f.read(2))[0]#11
+                        unknown6 = unpack("B", f.read(1))[0]#12
+                        unknown7 = unpack(">H", f.read(2))[0]#14
+                        unknown8 = unpack("B", f.read(1))[0]#15
+                        unknown9 = unpack(">H", f.read(2))[0]#17
+                        unknown10 = unpack("B", f.read(1))[0]#18
+                        unknown11 = unpack(">H", f.read(2))[0]#20
+                        unknown12 = unpack("B", f.read(1))[0]#21
+                        unknown13 = unpack(">H", f.read(2))[0]#23
+                        unknown14 = unpack("B", f.read(1))[0]#24
+                        unknown15 = unpack(">H", f.read(2))[0]#26
+                        unknown16 = unpack("B", f.read(1))[0]#27
+                        unknown17 = unpack(">H", f.read(2))[0]#29
+                        unknown18 = unpack("B", f.read(1))[0]#30
+                        unknown19 = unpack(">H", f.read(2))[0]#32
+                        unknown20 = unpack("B", f.read(1))[0]#33
+                        unknown21 = unpack(">H", f.read(2))[0]#35
+                        unknown22 = unpack("B", f.read(1))[0]#36
+                        #TODO negative 0x98 #38
+                        facecount = unpack("B", f.read(1))[0] # 39
+                        #facecount loop
+                        for i in range(facecount-2):
+                            fa = unpack(">I", f.read(4))[0] >> 16
+                            f.seek(2,1)
+                            fb = unpack(">I", f.read(4))[0] >> 16
+                            f.seek(2,1)
+                            fc = unpack(">I", f.read(4))[0] >> 16
+                            f.seek(2,1)
+                            f.seek(-12,1)
                     #needs a loop
                         
                                                             
     mesh = bpy.data.meshes.new("dragonjan")
-    mesh.from_pydata(vertices, [], [])
+    mesh.from_pydata(vertices, [], faces)
     object = bpy.data.objects.new("dragonjan", mesh)
     bpy.context.collection.objects.link(object)
 
